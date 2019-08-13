afba912d79d52aae0602b73f7a7fda71de0e062e
SitaNya <sitanya@qq.com>
2019-08-13 20:52:53 +0800

    init

---
+++ swing/src/main/java/com/test/Frame.java
@@ -0,0 +1,40 @@
+package com.test;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author SitaNya
+ * @date 2019-08-13
+ * @email sitanya@qq.com
+ * @qqGroup 162279609
+ * 有任何问题欢迎咨询
+ * <p>
+ * 类说明:
+ */
+public class Frame {
+    private String title;
+    private int width;
+    private int height;
+
+    public Frame(String title, int width, int height) {
+        this.title = title;
+        this.width = width;
+        this.height = height;
+    }
+
+    public JFrame init() {
+        JFrame f = new JFrame(title);
+        f.setSize(width, height);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+        f.setLayout(null);
+//        f.setLayout(new GridLayout(0,4));
+        try {
+            javax.swing.UIManager.setLookAndFeel(javax.swing.plaf.nimbus.NimbusLookAndFeel.class.getName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return f;
+    }
+}
---
+++ swing/src/main/java/com/test/Swing.java
@@ -0,0 +1,23 @@
+package com.test;
+
+/**
+ * @author SitaNya
+ * @date 2019-08-12
+ * @email sitanya@qq.com
+ * @qqGroup 162279609
+ * 有任何问题欢迎咨询
+ * <p>
+ * 类说明:
+ */
+//练习网格布局
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Swing extends JFrame {
+
+    public static void main(String[] args) {
+        new Tools().init();
+    }
+
+}
